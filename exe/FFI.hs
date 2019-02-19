e1fb848198fa086e6b927d2f66d90eeb758b7273
Maurice Doepke <mauricepdoepke@gmail.com>
2019-02-19 17:43:08 +0100

    test

--- exe/FFI.hs
+++ exe/FFI.hs
@@ -1,44 +1,8 @@
--- Copyright (c) 2016-present, Facebook, Inc.
--- All rights reserved.
---
--- This source code is licensed under the BSD-style license found in the
--- LICENSE file in the root directory of this source tree. An additional grant
--- of patent rights can be found in the PATENTS file in the same directory.
+{-# LANGUAGE ForeignFunctionInterface #-}
+module FFI where
+ 
+import Foreign.C.Types
 
-
-{-# LANGUAGE OverloadedStrings #-}
-
-import Data.Aeson
-import Data.ByteString (ByteString, empty)
-import Data.HashMap.Strict (HashMap)
-import Data.Maybe
-import Data.String
-import Data.Text (Text)
-import Data.Time.LocalTime.TimeZone.Series
-import Prelude
-import System.Directory
-import System.Environment (lookupEnv)
-import TextShow
-import Text.Read (readMaybe)
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.HashMap.Strict as HashMap
-import qualified Data.Text as Text
-import qualified Data.Text.Encoding as Text
-
-import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
-
-import Duckling.Core
-import Duckling.Data.TimeZone
-import Duckling.Resolve (DucklingTime)
-
-
-triple :: Int -> Int
-triple x = 3*x
-
-foreign export ccall triple :: Int -> Int
-
--- | Return which languages have which dimensions
-targets = HashMap.fromList . map dimText $ HashMap.toList supportedDimensions
-  where
-    dimText :: (Lang, [Some Dimension]) -> (Text, [Text])
-    dimText = (Text.toLower . showt) *** map (\(This d) -> toName d)
+foreign export ccall hs_fact :: CInt -> CInt
+hs_fact :: CInt -> CInt
+hs_fact n = product [1..n]
